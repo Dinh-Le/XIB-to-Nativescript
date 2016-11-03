f48eeafbc2160d2791ff8b739208f4c7fb5cf0e0
Le Dinh <dinh.le@smarp.com>
2016-11-03 10:07:31 +0200

    Revert "Swift Icon"
    This reverts commit 93488b75b2ae16956184307003ebaa6fd7d7bc3b.

--- Pods/Target Support Files/Pods-Pods-SGSnackBar_Tests/Pods-Pods-SGSnackBar_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Pods_SGSnackBar_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_Pods_SGSnackBar_Tests
-@end
--- Pods/Target Support Files/Pods-SGSnackBar-SGSnackBar/Pods-SGSnackBar-SGSnackBar-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SGSnackBar_SGSnackBar : NSObject
-@end
-@implementation PodsDummy_Pods_SGSnackBar_SGSnackBar
-@end
